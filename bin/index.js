08b23ea1604bdcdd17782f82fc54031b5112390e
muyi <yfdezhuye@163.com>
2014-12-06 21:31:24 +0800

    change files structor

---
+++ bin/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 14-12-6.
+ */
+var app = require('../server/app.js');
+
+app();
---
+++ server/routes.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 14-12-6.
+ */
+module.exports = function(app){
+
+};
